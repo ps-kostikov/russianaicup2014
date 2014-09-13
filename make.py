1bfd00b6376852885ed660a36229695d7d7d582a
Pavel Kostikov <pkostikov@yandex-team.ru>
2014-09-13 20:16:12 +0400

    upgrade package build

--- make.py
+++ make.py
@@ -3,9 +3,13 @@ import os
 
 files = [
     'src/geometry.py',
+    'src/geometry.py',
     'strategies/best/MyStrategy.py',
 ]
 
 with zipfile.ZipFile('strategy.zip', 'w') as out:
-    for f in files:
-        out.write(f, os.path.basename(f))
+    for src_file in filter(lambda f: f.endswith('.py'), os.listdir('src')):
+        out.write(os.path.join('src', src_file), src_file)
+
+    strategy_file = 'strategies/best/MyStrategy.py'
+    out.write(strategy_file, os.path.basename(strategy_file))
