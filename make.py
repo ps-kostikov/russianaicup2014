ee96e2df30626f9c97108a0059c96232d60e1a6f
Pavel Kostikov <pkostikov@yandex-team.ru>
2014-09-27 13:11:40 +0400

    fix make

--- make.py
+++ make.py
@@ -1,12 +1,6 @@
 import zipfile
 import os
 
-files = [
-    'src/geometry.py',
-    'src/geometry.py',
-    'strategies/best/MyStrategy.py',
-]
-
 with zipfile.ZipFile('strategy.zip', 'w') as out:
     for src_file in filter(lambda f: f.endswith('.py'), os.listdir('src')):
         out.write(os.path.join('src', src_file), src_file)
